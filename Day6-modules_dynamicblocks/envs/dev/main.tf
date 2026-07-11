61d0e381f8f72baac88a8b1676f403025aa2267d
naveen-672 <190330175cse@gmail.com>
2026-07-11 23:07:41 +0530

    Day6

--- Day6-modules_dynamicblocks/envs/dev/main.tf
+++ Day6-modules_dynamicblocks/envs/dev/main.tf
@@ -1,18 +1,18 @@
 module "dev_instance" {
-  source = "/root/envs/dev/compute"
+  source = "./compute"
   iname  = "dev-instance"
   ami_id = "ami-04ea4e8270c27626c"
   itype  = "t3.micro"
 }
 
 module "dev_security_group" {
-  source         = "/root/envs/dev/security_group"
+  source         = "./security_group"
   sg_name        = "dev_sg"
   sg_description = "this is my dev sg"
   ports          = [22, 80, 9000, 8080, 3306]
 }
 
 module "dev_bucket" {
-  source      = "/root/envs/dev/storage"
+  source      = "./storage"
   bucket_name = "dev.devops77"
 }
